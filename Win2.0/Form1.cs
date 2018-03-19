095c14a1bc069026d8dd37b4b3168fe535b87da8
Thura <Thura@LAPTOP-LF5G0E0E>
2018-03-19 17:25:56 +0630

    Add project files.

---
+++ Win2.0/Form1.cs
@@ -0,0 +1,254 @@
+﻿using IWshRuntimeLibrary;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Security.Principal;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Win2._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //CheckStartUpIn2();
+
+            //string path = "C:\\Evantek\\Move\\Run.bat";
+            //Process process = new Process()
+            //{
+            //    StartInfo = new ProcessStartInfo(path)
+            //    {
+            //        WorkingDirectory = Path.GetDirectoryName(path)
+            //    }
+            //};
+            //process.Start();
+            //process.WaitForExit();
+
+            string displaycontroller_path = "C:\\Evantek\\DBSController\\Evantek_Display.exe";
+            if (System.IO.File.Exists(displaycontroller_path))
+            {
+                Process displaycontroller_process = new Process()
+                {
+                    StartInfo = new ProcessStartInfo(displaycontroller_path)
+                    {
+                        WorkingDirectory = Path.GetDirectoryName(displaycontroller_path)
+                    }
+                };
+                displaycontroller_process.Start();
+                displaycontroller_process.WaitForExit();
+            }
+        }
+
+        private void CheckStartUpIn2()
+        {
+            string allusuerstr = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + @"\Microsoft\Windows\Start Menu\Programs\Startup";
+            string userstr = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+            MessageBox.Show(allusuerstr);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //RemoveFolder(@"C:\Move");
+
+            //CheckShorutcut();
+
+            Process[] processes_move = Process.GetProcessesByName("AutoIt3");
+            foreach (Process process in processes_move)
+            {
+                process.Kill();
+                process.WaitForExit();
+            }
+        }
+        private void RemoveFolder(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                DirectoryInfo mainfolder = new DirectoryInfo(path);
+                DirectoryInfo[] subfolder = mainfolder.GetDirectories();
+
+                foreach (var folder in subfolder)
+                {
+                    this.RemoveFolder(folder.FullName);
+                }
+
+                FileInfo[] files = mainfolder.GetFiles();
+                foreach (var file in files)
+                {
+                    file.Delete();
+                }
+                Directory.Delete(path);
+            }
+        }
+        private void CheckShorutcut()
+        {
+            string alluserstartup = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + @"\Microsoft\Windows\Start Menu\Programs\Startup";
+            string userstartup = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+
+            List<string> lststrup = new List<string>() { alluserstartup, userstartup };
+            List<string> lstshortcut = new List<string>() {
+                "Run.lnk", "Evantek_Display.lnk","Evantek_Display.exe - Shortcut.lnk","Evantek_Display - Shortcut.lnk","Evantek_Display.exe","Run.bat - Shortcut.lnk","Run - Shortcut.lnk","Run.bat","Delay_Launcher.exe - Shortcut.lnk","Delay_Launcher - Shortcut.lnk","Delay_Launcher.exe"
+            };
+
+            int count = 0;
+            foreach (var str in lststrup)
+            {
+                foreach (var shortcut in lstshortcut)
+                {
+                    string shorcut_path = Path.Combine(str, shortcut);
+                    if (System.IO.File.Exists(shorcut_path))
+                    {
+                        count = count + 1;
+                    }
+                    //System.IO.File.Delete(str + "\\" + shortcut);
+                }
+            }
+
+            MessageBox.Show(count.ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string shortcut_file_path = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + @"\Microsoft\Windows\Start Menu\Programs\Startup" + "\\test.lnk";
+            string soruce_file_path = @"C:\sfp\a.txt";
+            WshShellClass WshShell = new WshShellClass();
+            IWshShortcut shortcut = (IWshShortcut)WshShell.CreateShortcut(shortcut_file_path);
+            shortcut.TargetPath = soruce_file_path;
+            shortcut.WorkingDirectory = @"C:\sfp\";
+            //shortcut.Description = "EQMS Display Controller";
+            //shortcut.IconLocation = @"icon path";
+            shortcut.Save();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show(IsUserAdministrator().ToString());
+            ClearRegisteryUninstallHistory();
+            string UninstallCommandString = "/x {0} /qn";
+
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            process.StartInfo = startInfo;
+
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardError = true;
+
+            startInfo.FileName = "msiexec.exe";
+            startInfo.Arguments = string.Format(UninstallCommandString, "{B4034ED9-0C93-4BAB-9F92-521163F064AD}");
+
+            process.Start();
+
+
+
+            //TerminateAllProcess();
+            //ClearRegisteryUninstallHistory();
+        }
+        private void ClearRegisteryUninstallHistory()
+        {
+            const string Win32Loc = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
+            const string Win64Loc = @"Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall";
+
+            RegistryKey rk32 = Registry.LocalMachine.OpenSubKey(Win32Loc, true);
+            RegistryKey rk64 = Registry.LocalMachine.OpenSubKey(Win64Loc, true);
+
+            string DisplayNameValue = "EQMS";
+
+            if (rk64 != null)
+            {
+                using (RegistryKey rk = rk64)
+                {
+                    foreach (string skName in rk.GetSubKeyNames())
+                    {
+                        using (RegistryKey sk = rk.OpenSubKey(skName, false))
+                        {
+                            if (sk.GetValue("DisplayName") != null && Convert.ToString(sk.GetValue("DisplayName")).StartsWith(DisplayNameValue))
+                            {
+                                MessageBox.Show("productcode " + skName);
+                                //rk.DeleteSubKey(skName);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (rk32 != null)
+            {
+                using (RegistryKey rk = rk32)
+                {
+                    foreach (string skName in rk.GetSubKeyNames())
+                    {
+                        using (RegistryKey sk = rk.OpenSubKey(skName, false))
+                        {
+                            if (sk.GetValue("DisplayName") != null && Convert.ToString(sk.GetValue("DisplayName")).StartsWith(DisplayNameValue))
+                            {
+                                //rk.DeleteSubKey(skName);
+                                MessageBox.Show("productcode " + skName);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            MessageBox.Show("Done Registery Clean");
+        }
+
+        private void TerminateAllProcess()
+        {
+            //terminate controller process
+            Process[] processes_controller = Process.GetProcessesByName("Evantek_Display");
+            foreach (Process process in processes_controller)
+            {
+                process.Kill();
+                process.WaitForExit();
+            }
+
+            //terminate mouse move process
+            Process[] processes_move = Process.GetProcessesByName("AutoIt3");
+            foreach (Process process in processes_move)
+            {
+                process.Kill();
+                process.WaitForExit();
+            }
+
+            //terminate delaylauncher process
+            Process[] processes_delay = Process.GetProcessesByName("Delay_Launcher");
+            foreach (Process process in processes_delay)
+            {
+                process.Kill();
+                process.WaitForExit();
+            }
+            MessageBox.Show("Done Terminate");
+        }
+
+        public bool IsUserAdministrator()
+        {
+            bool isAdmin;
+            try
+            {
+                WindowsIdentity user = WindowsIdentity.GetCurrent();
+                WindowsPrincipal principal = new WindowsPrincipal(user);
+                isAdmin = principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                isAdmin = false;
+            }
+            catch (Exception ex)
+            {
+                isAdmin = false;
+            }
+            return isAdmin;
+        }
+
+    }
+}
---
+++ WindowForm/Form1.cs
@@ -0,0 +1,369 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServiceState s = ServiceInstaller.GetServiceStatus("BCS");
+            if (s.ToString() == "NotFound")
+            {
+                //register and start
+            }
+            else if (s.ToString() == "Running")
+            {
+                //stop 
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Evantek_Display
+            //AutoIt3
+            Process[] processes = Process.GetProcessesByName("Delay_Launcher");
+            foreach (Process process in processes)
+            {
+                process.Kill();
+                process.WaitForExit();
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Process[] processes = Process.GetProcessesByName("Evantek_Display");
+            foreach (Process process in processes)
+            {
+                process.Kill();
+                process.WaitForExit();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process[] lstprocess = Process.GetProcesses();
+            foreach (Process p in lstprocess)
+            {
+                label1.Text += p.ProcessName + "\r\n";
+            }
+            ReplaceController();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Process.Start("C:\\Custom\\WindowForm.exe");
+        }
+        private void DeleteFile(FileInfo[] Files)
+        {
+            foreach (FileInfo file in Files)
+            {
+                file.Delete();
+            }
+        }
+        private void DeleteDirectory(DirectoryInfo[] Directories)
+        {
+            foreach (DirectoryInfo dir in Directories)
+            {
+                dir.Delete(true);
+            }
+        }
+        private void CleanController()
+        {
+            string path = "C:/JInstaller/DBSContoller";
+            if (Directory.Exists(path))
+            {
+                DirectoryInfo di = new DirectoryInfo(path);
+                DeleteFile(di.GetFiles());
+                DeleteDirectory(di.GetDirectories());
+                //foreach (FileInfo file in di.GetFiles())
+                //{
+                //    file.Delete();
+                //}
+
+                //foreach (DirectoryInfo dir in di.GetDirectories())
+                //{
+                //    dir.Delete(true);
+                //}
+                Directory.Delete(path);
+            }
+        }
+        private void ReplaceController()
+        {
+            string source = "C:/cd/DBSContoller";
+            string destpath = "C:/dest/DBSContoller";
+            string backupdirectory = @"C:\Backup";
+
+
+
+            //if (Directory.Exists(destpath))
+            //{
+            //    DirectoryInfo di = new DirectoryInfo(destpath);
+            //    foreach (FileInfo file in di.GetFiles())
+            //    {
+            //        file.Delete();
+            //    }
+            //    foreach (DirectoryInfo dir in di.GetDirectories())
+            //    {
+            //        dir.Delete(true);
+            //    }
+            //    Directory.Delete(destpath);
+            //}
+
+            if (Directory.Exists(destpath))
+            {
+
+                //Directory.Move(destpath, backupdirectory + DateTime.Now.ToString("_MMMdd_yyyy_HHmmss"));
+                //Directory.Move(source, destpath);
+
+                foreach (var file in Directory.GetFiles(source))
+                {
+                    File.Copy(file, Path.Combine(destpath, Path.GetFileName(file)), false);
+                }
+
+
+            }
+        }
+
+        public void Test(dynamic obj)
+        {
+            string str = "";
+            Form1 frm = new Form1();
+            this.Test(str);
+            this.Test(frm);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //RemoveFolder("C:/Evantek");
+            RemoveSubDirectory("C:/Evantek",false);
+            //CopyFolder(@"C:\WorkSheet\InstallerSheet\NeedFilesandFolderInInstaller\Collection DBS Controller\LocalDBSController\19.10.2017", @"C:\Evantek");
+        }
+        private void RemoveFolder(string path)
+        {
+            DirectoryInfo mainfolder = new DirectoryInfo(path);
+            DirectoryInfo[] subfolder = mainfolder.GetDirectories().ToArray();
+
+            foreach (var folder in subfolder)
+            {
+                this.RemoveFolder(folder.FullName);
+            }
+
+            FileInfo[] files = mainfolder.GetFiles().ToArray();
+            foreach (var file in files)
+            {
+                file.Delete();
+            }
+            Directory.Delete(path);
+
+        }
+        private void RemoveSubDirectory(string path,bool issub)
+        {
+            DirectoryInfo mainfolder = new DirectoryInfo(path);
+            DirectoryInfo[] subfolder = mainfolder.GetDirectories().ToArray();
+
+            foreach (var folder in subfolder)
+            {
+                this.RemoveSubDirectory(folder.FullName,true);
+            }
+
+            if (issub)
+            {
+                FileInfo[] files = mainfolder.GetFiles().ToArray();
+                foreach (var file in files)
+                {
+                    file.Delete();
+                }
+                Directory.Delete(path);
+            }
+        }
+        private void CopyFolder2(DirectoryInfo source, DirectoryInfo target, bool isRoot)
+       {
+            foreach (DirectoryInfo dir in source.GetDirectories())
+                CopyFolder2(dir, target.CreateSubdirectory(dir.Name), false);
+            if (!isRoot)
+            {
+                foreach (FileInfo file in source.GetFiles())
+                    file.CopyTo(Path.Combine(target.FullName, file.Name), true);
+            }
+        }
+        private void CopyFolder(string SourcePath, string DestinationPath)
+        {
+            //Now Create all of the directories
+            foreach (string dirPath in Directory.GetDirectories(SourcePath, "*",SearchOption.AllDirectories))
+                Directory.CreateDirectory(dirPath.Replace(SourcePath, DestinationPath));
+
+            //Copy all the files & Replaces any files with the same name
+            foreach (string newPath in Directory.GetFiles(SourcePath, "*.*",SearchOption.AllDirectories))
+                File.Copy(newPath, newPath.Replace(SourcePath, DestinationPath), true);
+
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show(this.GetUninstallString("EQMS Display Controller - 1.0.32"));
+            //regtest2();
+            regRefracted();
+        }
+        public void regtest2()
+        {
+            const string Win32Loc = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
+                                    
+            const string Win64Loc = @"Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall";
+
+            List<Application> apps = new List<Application>();
+            //string location = bool64BitOs ? Win64Loc : Win32Loc;
+            string location = Win64Loc;
+
+            using (RegistryKey rk = Registry.LocalMachine.OpenSubKey(location,false))
+            {
+                foreach (string skName in rk.GetSubKeyNames())
+                {
+                    
+                    using (RegistryKey sk = rk.OpenSubKey(skName,false))
+                    {
+                        //if (sk.GetValue("DisplayName") != null && Convert.ToString(sk.GetValue("DisplayName"))== "EQMS Display Controller - 1.0.32")
+                        if (sk.GetValue("DisplayName") != null && Convert.ToString(sk.GetValue("DisplayName")).Contains("EQMS Display Controller -") )
+                        {
+                            
+                            string s = sk.GetValue("DisplayName").ToString();
+                            //rk.DeleteValue(skName);
+                            break;
+
+                        }
+                        //apps.Add(new Application { DisplayName = sk.GetValue("DisplayName") == null ? "" : (string)sk.GetValue("DisplayName"), AppKey = skName });
+                    }
+                }
+            }
+
+        }
+        private void regRefracted()
+        {
+            const string Win32Loc = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
+            const string Win64Loc = @"Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall";
+
+            RegistryKey rk32 = Registry.LocalMachine.OpenSubKey(Win32Loc,true);
+            RegistryKey rk64 = Registry.LocalMachine.OpenSubKey(Win64Loc,true);
+
+            string DisplayNameValue = "EQMS";
+
+            if (rk64 != null)
+            {
+                using (RegistryKey rk = rk64)
+                {
+                    foreach (string skName in rk.GetSubKeyNames())
+                    {
+                        using (RegistryKey sk = rk.OpenSubKey(skName, false))
+                        {
+                            if (sk.GetValue("DisplayName") != null && Convert.ToString(sk.GetValue("DisplayName")).StartsWith(DisplayNameValue))
+                            {
+
+                                string s = sk.GetValue("DisplayName").ToString();
+                                rk.DeleteSubKey(skName);
+                                //rk.DeleteValue(skName);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (rk32 != null)
+            {
+                using (RegistryKey rk = rk32)
+                {
+                    foreach (string skName in rk.GetSubKeyNames())
+                    {
+                        using (RegistryKey sk = rk.OpenSubKey(skName, false))
+                        {
+                            if (sk.GetValue("DisplayName") != null && Convert.ToString(sk.GetValue("DisplayName")).StartsWith(DisplayNameValue))
+                            {
+                                string s = sk.GetValue("DisplayName").ToString();
+                                rk.DeleteSubKey(skName);
+                                //rk.DeleteValue(skName);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+        public void deleteregistersetting()
+        {
+            //try
+            //{
+            //    RegistryKey rkApp = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            //    if (rkApp.GetValue("EQMS Display Controller - 1.0.32") != null)
+            //    {
+            //        string s = "ok";
+            //        //rkApp.DeleteValue("Steve");
+            //        //rkApp.DeleteSubKey("Steve", false);
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    string msg = ex.Message.ToString();
+            //}
+        }
+        private string GetUninstallString(string msiName)
+        {
+            //Utility.WriteLog("Entered GetUninstallString(msiName) - Parameters: msiName = " + msiName);
+            string uninstallString = string.Empty;
+            try
+            {
+                //string path = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Installer\\UserData\\S-1-5-18\\Products";
+                string path = "SOFTWARE\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\{3484D541-E9A8-4455-A1C3-93686961D347}";
+                RegistryKey key = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64).OpenSubKey(path);
+                //RegistryKey key = Registry.LocalMachine.OpenSubKey(path);
+
+                
+
+                foreach (string tempKeyName in key.GetSubKeyNames())
+                {
+                    RegistryKey tempKey = key.OpenSubKey(tempKeyName + "\\InstallProperties");
+                    if (tempKey != null)
+                    {
+                        //string s= Convert.ToString(tempKey.GetValue("EQMS Display Controller - 1.0.32"));
+                        if (string.Equals(Convert.ToString(tempKey.GetValue("EQMS Display Controller - 1.0.32")), msiName, StringComparison.CurrentCultureIgnoreCase))
+                        {
+                            uninstallString = Convert.ToString(tempKey.GetValue("UninstallString"));
+                            uninstallString = uninstallString.Replace("/I", "/X");
+                            uninstallString = uninstallString.Replace("MsiExec.exe", "").Trim();
+                            uninstallString += " /quiet /qn";
+                            break;
+                        }
+                    }
+                }
+
+                return uninstallString;
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException(ex.Message);
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo source = new DirectoryInfo(@"C:\TestDest");
+            DirectoryInfo target = new DirectoryInfo(@"C:\Evantek");
+            CopyFolder2(source,target,true);
+        }
+    }
+}
